0adff58ccb82f3c1602cb2376e62bbf8de8188e0
aholt@rsmas.miami.edu <aholt@rsmas.miami.edu>
2026-07-23 16:51:39 -0400

    style: T/plate-thickness panel filled with DP color scale, inline white contour labels

--- analysis/observations/plot_final_map.py
+++ analysis/observations/plot_final_map.py
@@ -258,37 +258,38 @@ plate_thick_vals_km = plate_thick_vals / 1e3
 # Create the subplot (ax3) for the colored grid.
 ax3 = plt.subplot(G[0, 2])
 
+# filled background shares the DP map's color scale (plasma_r, 10-60 MPa)
+ax3.contourf(T_vals, plate_thick_vals_km, DPmean_thresh_grid,
+             levels=np.linspace(DPmin, DPmax, 251), cmap='plasma_r', norm=norm1)
 contour_levels1 = np.arange(20, 40+2, 2)
-contour_levels1_minor = np.arange(21, 40, 2)
-contours1 = ax3.contour(T_vals, plate_thick_vals_km, DPmean_thresh_grid, levels=contour_levels1, colors='black', linewidths=1, linestyles='solid')
-labels1 = ax3.clabel(contours1, levels=contour_levels1, inline=False, fontsize=8, fmt='%d')
-for txt in labels1:
-    txt.set_bbox(dict(boxstyle='square,pad=0.1', fc='white', ec='none'))
-ax3.contour(T_vals, plate_thick_vals_km, DPmean_thresh_grid, levels=contour_levels1_minor, colors='black', linewidths=0.5, linestyles='dashed')
-
-def _rightmost_label(ax, cs, level, xlim, ylim, color, fontsize=8):
-    lidx = np.where(np.isclose(cs.levels, level))[0]
-    if len(lidx) == 0:
-        return
-    best_x, best_y = -np.inf, None
-    for seg in cs.allsegs[lidx[0]]:
-        mask = (seg[:,0] >= xlim[0]) & (seg[:,0] <= xlim[1]) & \
-               (seg[:,1] >= ylim[0]) & (seg[:,1] <= ylim[1])
-        if mask.any():
-            pts = seg[mask]
-            i = np.argmax(pts[:,0])
-            if pts[i,0] > best_x:
-                best_x, best_y = pts[i,0], pts[i,1]
-    if best_y is not None:
-        ax.text(best_x, best_y, str(int(level)), fontsize=fontsize, color=color,
-                ha='right', va='bottom',
-                bbox=dict(boxstyle='square,pad=0.1', fc='white', ec='none'))
-
-_rightmost_label(ax3, contours1, 30, (1250, 1450), (80, 135), 'black')
-_rightmost_label(ax3, contours1, 32, (1250, 1450), (80, 135), 'black')
+contours1 = ax3.contour(T_vals, plate_thick_vals_km, DPmean_thresh_grid,
+                        levels=contour_levels1, colors='white', linewidths=0.9)
+# one label per line, at the midpoint of its longest VISIBLE portion
+xlim3, ylim3 = (1250, 1450), (80, 135)
+pad_x = 0.04 * (xlim3[1] - xlim3[0])
+pad_y = 0.04 * (ylim3[1] - ylim3[0])
+label_pts = []
+for segs in contours1.allsegs:
+    best = None
+    for s in segs:
+        vis = s[(s[:,0] >= xlim3[0]+pad_x) & (s[:,0] <= xlim3[1]-pad_x) &
+                (s[:,1] >= ylim3[0]+pad_y) & (s[:,1] <= ylim3[1]-pad_y)]
+        if len(vis) > 1 and (best is None or len(vis) > len(best)):
+            best = vis
+    if best is not None:
+        # start at the midpoint; if too close to the star, walk outward until clear
+        def _near_star(p, thresh=0.09):
+            return (((p[0]-Tm)/(xlim3[1]-xlim3[0]))**2 +
+                    ((p[1]-plate_thick/1e3)/(ylim3[1]-ylim3[0]))**2) < thresh**2
+        mid = len(best)//2
+        order = sorted(range(len(best)), key=lambda i: abs(i-mid))
+        pick = next((i for i in order if not _near_star(best[i])), mid)
+        label_pts.append(tuple(best[pick]))
+ax3.clabel(contours1, inline=True, inline_spacing=2, fontsize=8, fmt='%d', manual=label_pts)
 
 # plot paramters of the map
-ax3.plot(Tm, plate_thick/1e3, 'k*', markersize=9)
+ax3.plot(Tm, plate_thick/1e3, marker='*', color='black', markersize=11,
+         markeredgecolor='white', markeredgewidth=0.7)
 
 # Set axis labels and ticks.
 ax3.set_xlabel('Basal temperature  [°C]')
